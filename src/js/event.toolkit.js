24dbf5b867b849c0df8247e7ebfdfca568307b8a
sabeur <thabti.sabeur@gmail.com>
2014-12-14 00:07:14 +0000

    Version bump for release

---
+++ src/js/event.toolkit.js
@@ -0,0 +1,11 @@
+var event = require('./event');
+
+if (typeof toolkit === "undefined") window.toolkit = {};
+if (typeof window.define === "function" && window.define.amd) {
+    define('bower_components/bskyb-event/dist/js/event.toolkit', [], function() {
+        'use strict';
+        return event;
+    });
+} else {
+    toolkit.detect = event;
+}
