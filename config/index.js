faead1c409c036e10d2accf94e140302566be5f2
Peter Mouland <peter.mouland@gmail.com>
2014-12-16 17:18:11 +0000

    Version bump for release

--- config/index.js
+++ config/index.js
@@ -6,9 +6,9 @@ module.exports = {
     bower: bower,
     aws:{
         release: true,
-        bucket: process.env.TOOLKIT_AWS_SKYGLOBAL_BUCKET,
-        key: process.env.TOOLKIT_AWS_ACCESS_KEY_ID,
-        secret: process.env.TOOLKIT_AWS_SECRET_ACCESS_KEY,
-        region: process.env.TOOLKIT_AWS_REGION
+        bucket: process.env.AWS_SKYGLOBAL_BUCKET,
+        key: process.env.AWS_ACCESS_KEY_ID,
+        secret: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION
     }
 };
