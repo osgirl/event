1fccf83ca9e35d4488b8900aab76bb9c5c7245df
Peter Mouland <peter.mouland@gmail.com>
2014-12-17 14:17:22 +0000

    remove un-needed gulp task + extra event.js file

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,3 @@ var gulp = require('gulp');
 var componentHelper = require('gulp-component-helper')(gulp);
 var paths = componentHelper.paths;
 var runSequence = require('run-sequence');
-
-var uglify = require('gulp-uglify');
-
-
-gulp.task('pre-build', function(){
-//    non common js file
-    gulp.src('src/js/event.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js/events'))
-});
-
